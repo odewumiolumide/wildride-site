f17361aafa25331a18559add981da9e0ec5cc87a
Odewumi Olumide <96873080+odewumiolumide@users.noreply.github.com>
2025-03-24 21:07:01 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_PzdcwlS4c', // Your User Pool ID
-        userPoolClientId: 'kk76ht8ts26lg5ant5o18vbph', // Your Client ID
-        region: 'us-east-1' // Your AWS Region
+        userPoolId: 'us-east-1_KSjuB7EM6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2rh1oe2adnpkbj54jsbad92vku', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // Your API invoke URL if needed
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
