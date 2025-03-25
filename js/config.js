8336d4921349dbf9fa3a4e07c72c4403987a0ae1
Odewumi Olumide <96873080+odewumiolumide@users.noreply.github.com>
2025-03-24 20:39:56 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_PzdcwlS4c', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'kk76ht8ts26lg5ant5o18vbph', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: us-east-1'' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
